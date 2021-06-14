742e656d6f75a918575d3268fe989366cf2119a9
BEASTELMO <beastelmo8@gmail.com>
2021-06-14 15:17:07 -0400

    front being worked on

--- geo_app/src/App.js
+++ geo_app/src/App.js
@@ -2,8 +2,31 @@ import { Component } from 'react'
 import Header from './components/Header'
 import Main_window from './components/Main_window'
 
-class App extends Component  {
-  render(){
+const baseURL = 'http://localhost:3003'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      reviews: [],
+      newReview: false,
+      editReview: false,
+      showReview: true,
+      displayedReview: {},
+    }
+    this.getReviews = this.getReviews.bind(this)
+  }
+
+  componentDidMount() {
+    this.getReviews()
+  }
+  getReviews() {
+    fetch(baseURL + '/reviews')
+      .then(data => { return data.json() }, err => console.log(err))
+      .then(parsedData => this.setState({ reviews: parsedData }), err => console.log(err))
+  }
+
+  render() {
     return (
       <div>
         < Header />
