ed2c29d3ad33927a6f281499e75cbab97a8f3567
Hannah Gardner <hannahgardner@Hannahs-MBP.lan>
2021-06-15 09:44:28 -0500

    centerMaps updated

--- geo_app/src/components/CenterMaps.js
+++ geo_app/src/components/CenterMaps.js
@@ -1,6 +1,37 @@
 import { Component } from 'react'
+import {Map, GoogleApiWrapper} from "google-maps-react"
+
+const mapStyles = {
+    width: '100%',
+    height: '100%'
+  }
 
 class CenterMaps extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            // http request = locationURL + output + input + search text + inputtype + optional params + apikey
+            locationURL: "https://maps.googleapis.com/maps/api/place/findplacefromtext/",
+            output: "json?",
+            input: "?input=",
+            searchText: "", // spaces must be replaced w "%20" - filter?
+            inputType: "&inputtype=textquery",
+            keyword: "&keyword=restaurant",
+            apiKey: "&key=AIzaSyDe6WRFuz0lxAuAJ1ZfiviZuQXRrtQCjc0",
+            lat: "",
+            lng: "",
+            location: {}
+        }
+    }
+    setSearchText = () => {
+
+    }
+
+    searchRestaurant = () => {
+        fetch(locationURL + output + input + searchText + inputType + keyword + apiKey)
+            .then(data => { return data.json() }, err => console.log(err))
+            .then(parsedData => this.setState({ location: parsedData }), err => console.log(err))
+    }
     render() {
         return (
             <div class="centermaps flexitem">
@@ -8,6 +39,12 @@ class CenterMaps extends Component {
                 <iframe className = "mapView">
                     
                 </iframe>
+                {/* <Map 
+                    google = {this.props.google}
+                    xoom = {8}
+                    style = {mapStyles}
+                    initialCenter = {{lat: Number(this.state.lat), lng: Number(this.state.lng)}}
+                ></Map> */}
             </div>
         )
     }
