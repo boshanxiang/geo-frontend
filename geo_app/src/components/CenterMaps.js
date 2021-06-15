c2b7ab48128b33084ec86165112c624933513557
Hannah Gardner <hannahgardner@Hannahs-MBP.lan>
2021-06-15 10:22:44 -0500

    work on maps api

--- geo_app/src/components/CenterMaps.js
+++ geo_app/src/components/CenterMaps.js
@@ -6,13 +6,13 @@ const mapStyles = {
     height: '90%'
 }
 
-class CenterMaps extends Component {
+export class MapContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
             // http request = locationURL + output + input + search text + inputtype + optional params + apikey
             locationURL: "https://maps.googleapis.com/maps/api/place/findplacefromtext/",
-            output: "json?",
+            output: "json",
             input: "?input=",
             searchText: "mcdonalds", // spaces must be replaced w "%20" - filter?
             inputType: "&inputtype=textquery",
@@ -20,7 +20,7 @@ class CenterMaps extends Component {
             apiKey: "&key=AIzaSyDe6WRFuz0lxAuAJ1ZfiviZuQXRrtQCjc0",
             lat: "",
             lng: "",
-            location: {}
+            location: []
         }
     }
     setSearchText = () => {
@@ -28,28 +28,32 @@ class CenterMaps extends Component {
     }
 
     searchRestaurant = () => {
+        console.log(this.state.locationURL + this.state.output + this.state.input + this.state.searchText + this.state.inputType + this.state.keyword + this.state.apiKey)
         fetch(this.state.locationURL + this.state.output + this.state.input + this.state.searchText + this.state.inputType + this.state.keyword + this.state.apiKey)
             .then(data => { return data.json() }, err => console.log(err))
             .then(parsedData => this.setState({ location: parsedData }), err => console.log(err))
+        console.log(this.state.location)
     }
 
     render() {
         return (
-            <div class="centermaps flexitem">
-                <h1>Center Maps </h1>
-                <iframe className = "mapView">
+            // <div class="centermaps flexitem">
+            //     <h1>Center Maps </h1>
+            //     <iframe className = "mapView">
                     
-                </iframe>
-                {/* <Map 
+            //     </iframe>
+                <Map 
                     google = {this.props.google}
                     xoom = {8}
                     style = {mapStyles}
-                    initialCenter = {{lat: Number(this.state.lat), lng: Number(this.state.lng)}}
-                ></Map> */}
-                <button onClick = {() => this.searchRestaurant}>FETCH</button>
-            </div>
+                    initialCenter = {{lat: -1.2884, lng: 36.8233}}
+                ></Map>
+                // <button onClick = {() => this.searchRestaurant()}>FETCH</button>
+            // </div>
         )
     }
 }
 
-export default CenterMaps 
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyDe6WRFuz0lxAuAJ1ZfiviZuQXRrtQCjc0'
+  })(MapContainer)
