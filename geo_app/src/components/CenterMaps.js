5ced1d26c336c8834399aacc31b003e08950d83d
Bo-Shan Xiang <bo-shanxiang@Bo-Shans-MBP.lan>
2021-06-15 11:37:31 -0500

    markers and infowindow added to google maps component

--- geo_app/src/components/CenterMaps.js
+++ geo_app/src/components/CenterMaps.js
@@ -1,12 +1,37 @@
 import { Component } from 'react'
-import {Map, GoogleApiWrapper} from "google-maps-react"
+import {Map, GoogleApiWrapper, InfoWindow, Marker} from "google-maps-react"
 
 const mapStyles = {
-    width: '25%',
-    height: '50%'
+    width: '35%',
+    height: '60%'
 }
 
 export class MapContainer extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {}
+        }
+    }
+
+    onMarkerClick = (props, marker, e) =>
+    this.setState({
+      showingInfoWindow: true,
+      selectedPlace: props,
+      activeMarker: marker
+    });
+
+  onClose = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
 
     render() {
         return (
@@ -16,7 +41,20 @@ export class MapContainer extends Component {
                     xoom = {8}
                     style = {mapStyles}
                     initialCenter = {{lat: -1.2884, lng: 36.8233}}
-                ></Map>
+                >
+                    <Marker
+                        onClick={this.onMarkerClick}
+                        name={'Kenyatta International Convention Centre'}
+                        />
+                    <InfoWindow
+                        marker={this.state.activeMarker}
+                        visible={this.state.showingInfoWindow}
+                        onClose={this.onClose}
+                        >
+                        <h4>{this.state.selectedPlace.name}</h4>
+                    </InfoWindow>
+                </Map>
+
             </div>
         )
     }
---
+++ geo_app/src/components/Map.js
@@ -0,0 +1,16 @@
+import {Component} from 'react';
+
+const mapStyles = {
+  map: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  }
+};
+
+export class CurrentLocation extends Component {
+
+}
+
+export default CurrentLocation;
+ 
