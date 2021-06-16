59d9fef352c5a09012054fea5188dbdaa0bfd6e8
daniele-frade <63023863+danielefrade@users.noreply.github.com>
2021-06-16 14:25:38 -0400

    Fix header and footer style

--- geo_app/src/components/Header.js
+++ geo_app/src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
-                <h1>Header</h1>
+                <h1>Geo App</h1>
             </div>
         )
     }
